c6e55abcf03cb52fe70ed8d3ed1e2ea965ea468a
Elliot <eal133@georgetown.edu>
2024-12-04 00:21:05 -0500

    progress

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
 from web_app.routes.home_routes import home_routes
+from web_app.routes.auth_routes import auth_routes
+
+from authlib.integrations.flask_client import OAuth
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = SECRET_KEY
+
+    # for Flash
+    #app.config["SECRET_KEY"] = SECRET_KEY
+    
     app.register_blueprint(home_routes)
+    app.register_blueprint(auth_routes)
     return app
 
 if __name__ == "__main__":
