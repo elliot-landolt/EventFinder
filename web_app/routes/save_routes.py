66d744a42cb3d03b4012e798b2920557b364e578
Elliot <eal133@georgetown.edu>
2024-12-09 16:06:08 -0500

    creation logic

--- web_app/routes/save_routes.py
+++ web_app/routes/save_routes.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, session, render_template, current_app
+from flask import Blueprint, session, render_template, current_app, request, flash
+from app.database.models.itinerary import Itinerary
 
 save_routes = Blueprint("save_routes", __name__)
 
-@save_routes.route("/saved")
+@save_routes.route("/saved", methods=['POST', 'GET'])
 def saved():
+    if request.method == 'POST':
+        request_data = dict(request.form)
+        Itinerary.create({
+            "title":request_data.get('title', '').strip(),
+            "user_id":session['current_user']['email'],
+            "description":request_data.get('description', '').strip()
+        })
+        flash('Itinerary Added!', 'success')
+
+
     return render_template("saved.html")
 
 
