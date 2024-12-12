2cb07538e5b9f7af4cf7333518ee29970dd55bb1
Elliot <eal133@georgetown.edu>
2024-12-12 17:18:17 -0500

    bug fix

--- web_app/routes/wrapper.py
+++ web_app/routes/wrapper.py
@@ -2,7 +2,7 @@ import functools
 from flask import session, redirect, flash
 
 def authenticated_route(view):
-    @functools.wrap(view)
+    @functools.wraps(view)
     def wrapped_view(**kwargs):
         if session.get('current_user'):
             return view(**kwargs)
