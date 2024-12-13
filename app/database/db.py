9aa2aadd0738eed1916091e9f4aff6dbe7a067b3
Elliot <eal133@georgetown.edu>
2024-12-13 00:59:35 -0500

    prod update

--- app/database/db.py
+++ app/database/db.py
@@ -6,7 +6,7 @@ from gspread_models.base import BaseModel
 load_dotenv()
 
 DEFAULT_FILEPATH = os.path.join(os.path.dirname(__file__),'..','google-credentials.json')
-GOOGLE_CREDENTIALS_FILEPATH = os.getenv("GOOGLE_GREDENTIALS_FILEPATH", default=DEFAULT_FILEPATH)
+GOOGLE_CREDENTIALS_FILEPATH = os.getenv("GOOGLE_CREDENTIALS_FILEPATH", default=DEFAULT_FILEPATH)
 
 GOOGLE_SHEETS_DOCUMENT_ID = os.getenv('GOOGLE_SHEETS_DOCUMENT_ID', default="OOPS, Please get the correct Document ID")
 
